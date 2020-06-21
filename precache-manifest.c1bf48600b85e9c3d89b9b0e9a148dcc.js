a5508403c8fba7a165f2a35d4c246618c081bbdb
Bharadhwaj CN <bharadhwaj@recruiterbox.com>
2020-06-21 12:20:21 +0530

    Updates

---
+++ precache-manifest.c1bf48600b85e9c3d89b9b0e9a148dcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b488b02bac46b1eb5fdfa080dabe3b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e9d0923023b29c32de",
+    "url": "/static/js/2.890636ea.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.890636ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6045672bcc76b2054d3b",
+    "url": "/static/js/main.24578f0e.chunk.js"
+  },
+  {
+    "revision": "502bfc17e0a7e0eac655",
+    "url": "/static/js/runtime-main.9face088.js"
+  }
+]);
