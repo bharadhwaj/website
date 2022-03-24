137f0e88af6985500ebe1adde91ae7660f26b842
Bharadhwaj CN <bharadhwaj10@gmail.com>
2022-03-24 18:26:00 +0530

    Update the actions

--- src/actions/window.action.js
+++ src/actions/window.action.js
@@ -1,8 +1,33 @@
 import { actions } from '../constants';
 
-export const setActiveWindow = (window) => ({
+export const showWindow = (windowName) => ({
+  type: actions.WINDOW.SHOW_WINDOW,
+  payload: { windowName },
+});
+
+export const minimizeWindow = (windowName) => ({
+  type: actions.WINDOW.MINIMIZE_WINDOW,
+  payload: { windowName },
+});
+
+export const closeWindow = (windowName) => ({
+  type: actions.WINDOW.CLOSE_WINDOW,
+  payload: { windowName },
+});
+
+export const maximizeWindow = (windowName) => ({
+  type: actions.WINDOW.MAXIMIZE_WINDOW,
+  payload: { windowName },
+});
+
+export const setActiveWindow = (windowName) => ({
   type: actions.WINDOW.SET_ACTIVE_WINDOW,
-  payload: { window },
+  payload: { windowName },
+});
+
+export const minimizeAllExcept = (windowName) => ({
+  type: actions.WINDOW.MINIMIZE_ALL_EXCEPT,
+  payload: { windowName },
 });
 
 export const setExperienceWindowVisibility = (isVisible) => ({
@@ -48,9 +73,4 @@ export const setLinksWindowMinimize = (isMinimized) => ({
 export const setShutdownPopupVisibility = (isVisible) => ({
   type: actions.WINDOW.SET_SHUTDOWN_POPUP_VISIBILITY,
   payload: { isVisible },
-});
-
-export const minimizeAllExcept = (windowName) => ({
-  type: actions.WINDOW.MINIMIZE_ALL_EXCEPT,
-  payload: { windowName },
 });
