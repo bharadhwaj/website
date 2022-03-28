bf2deb9d38eea39120c35e0be89fe3eb21bd02ac
Bharadhwaj CN <bharadhwaj10@gmail.com>
2022-03-28 16:12:02 +0530

    Updates

---
+++ precache-manifest.893d904a7d3ba3a90464aca8d63bcfdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d308ba9bf9e352c947b6205562e6875",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4210f50093db80fbad",
+    "url": "/static/js/2.34ac1138.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.34ac1138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699bee06b42750655bf6",
+    "url": "/static/js/main.93febded.chunk.js"
+  },
+  {
+    "revision": "502bfc17e0a7e0eac655",
+    "url": "/static/js/runtime-main.9face088.js"
+  }
+]);
