a8747605ce4cac55b161c6dae89b60b7303d21b8
Bharadhwaj CN <bharadhwaj10@gmail.com>
2022-11-26 22:38:22 +0530

    Rewrite the components to tailwind css

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,31 +1,29 @@
 import React from "react";
 import { Button, Window, WindowContent, WindowHeader } from "react95";
 
-import style from "./style";
-
 import LogoIcon from "../LogoIcon";
 
 const Popup = (props) => {
   const { title, image, action, handleClose } = props;
 
-  const classes = style();
-
   return (
-    <Window className={classes.screen}>
-      <WindowHeader className={classes.header}>
-        <span>Shut Down Windows</span>
-        <Button className={classes.headerButton} size={"sm"} square onClick={handleClose}>
-          <span>x</span>
-        </Button>
-      </WindowHeader>
-      <WindowContent>
-        <div className={classes.content}>
-          <LogoIcon image={image} alt='' size={40} />
-          <span>{title}</span>
-        </div>
-        <div className={classes.action}>{action}</div>
-      </WindowContent>
-    </Window>
+    <div className='flex h-screen'>
+      <Window className='m-auto items-center md:w-[60%] lg:w-[30%]'>
+        <WindowHeader className='flex items-center justify-between'>
+          <span>Shut Down Windows</span>
+          <Button className='-ml-0.5 -mb-0.5 font-bold' size={"sm"} square onClick={handleClose}>
+            <span>x</span>
+          </Button>
+        </WindowHeader>
+        <WindowContent>
+          <div className='flex items-center'>
+            <LogoIcon className='w-10' image={image} alt='' />
+            <span>{title}</span>
+          </div>
+          <div className='flex items-center justify-end'>{action}</div>
+        </WindowContent>
+      </Window>
+    </div>
   );
 };
 
