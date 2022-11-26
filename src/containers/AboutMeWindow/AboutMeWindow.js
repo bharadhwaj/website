10728f1aa74c6889245632b8224288f469a95785
Bharadhwaj CN <bharadhwaj10@gmail.com>
2022-11-27 00:45:28 +0530

    Add tailwind for about me window

--- src/containers/AboutMeWindow/AboutMeWindow.js
+++ src/containers/AboutMeWindow/AboutMeWindow.js
@@ -1,9 +1,6 @@
 import React from "react";
-import clsx from "clsx";
 import { Button, Fieldset, Window, WindowContent, WindowHeader } from "react95";
 import Draggable from "react-draggable";
-
-import style from "./style";
 
 import { WINDOW } from "../../constants";
 
@@ -19,7 +16,8 @@ const AboutMeWindow = (props) => {
     closeWindow,
   } = props;
 
-  const classes = style();
+  const maximizedWindowClassName = "h-[calc(100%-45px)] min-w-full";
+  const normalWindowClassName = "h-[50%] max-w-[60%]";
 
   if (!isVisible || isMinimized) {
     return null;
@@ -27,46 +25,46 @@ const AboutMeWindow = (props) => {
 
   return (
     <Draggable
-      defaultClassName={clsx(activeWindow === WINDOW.ABOUT_ME && classes.activeWindow)}
+      defaultClassName={activeWindow === WINDOW.ABOUT_ME && "z-10"}
       bounds='parent'
       handle='header'
-      defaultPosition={{ x: 20, y: 40 }}
+      defaultPosition={{ x: 0, y: 0 }}
     >
       <Window
-        className={clsx(isMaximized ? classes.maximizedScreen : classes.minimizedScreen)}
+        className={isMaximized ? maximizedWindowClassName : normalWindowClassName}
         onClick={setAboutMeWindowActive}
       >
         <header>
-          <WindowHeader className={classes.headerArea}>
+          <WindowHeader className='flex items-center justify-between cursor-move'>
             <div>
               <span>about_me.exe</span>
             </div>
             <div>
-              <Button className={classes.actionButtons} size={"sm"} square onClick={minimizeWindow}>
+              <Button className='-ml-0.5 -mb-0.5 font-bold' size={"sm"} square onClick={minimizeWindow}>
                 <span>-</span>
               </Button>
-              <Button className={classes.actionButtons} size={"sm"} square onClick={toggleMaximizeWindow}>
+              <Button className='-ml-0.5 -mb-0.5 font-bold' size={"sm"} square onClick={toggleMaximizeWindow}>
                 <span>□</span>
               </Button>
-              <Button className={classes.actionButtons} size={"sm"} square onClick={closeWindow}>
+              <Button className='-ml-0.5 -mb-0.5' size={"sm"} square onClick={closeWindow}>
                 <span>x</span>
               </Button>
             </div>
           </WindowHeader>
         </header>
-        <WindowContent>
-          <Fieldset className={classes.aboutMe}>
-            <div className={classes.paragraph}>
-              Hey there. This is Bharadhwaj, from Palakkad, God\'s own country - Kerala. I'm a passionate web developer,
-              programmer and a B.Tech graduate in Computer Science and Engineering. I am passionate about Technology,
+        <WindowContent className='h-[100%]'>
+          <Fieldset className='h-[90%] md:min-h-fit text-base font-monospace overflow-y-scroll'>
+            <div className='mt-5 text-gray-600'>
+              Hey there. This is Bharadhwaj, from God's own country - Kerala. I'm a passionate web developer, programmer
+              and a B.Tech graduate in Computer Science and Engineering. I am passionate about Technology,
               Entrepreneurship and Startup.
             </div>
-            <div className={classes.paragraph}>
+            <div className='mt-5 text-gray-600'>
               Currently, I am living in Bangalore, working for Applied Training, a Talent Management platform, where I
               am trying to integrate multiple products of Applied under a single hood. My day to day work mostly consist
               of coding in Python Django and React.js.
             </div>
-            <div className={classes.paragraph}>
+            <div className='mt-5 text-gray-600'>
               If not at Work and if not sleeping, I spend time travelling and exploring new places. I follow Sports
               especially Cricket and Football and love to play outdoor games(even though I suck at most of them).
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      monospace: ["monospace", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
